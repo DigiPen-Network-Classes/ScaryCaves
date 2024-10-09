b951f2ae4c5dea46231aee0334537606b0359d31
Tony Rasa <trasa@meancat.com>
2024-10-09 13:48:44 -0700

    scary cave (html shell)

--- Models/RoomModel.cs
+++ Models/RoomModel.cs
@@ -1,6 +1,8 @@
 namespace ScaryCavesWeb.Models;
 
-public class RoomModel
+public class RoomModel(int id, string name, string description)
 {
-    
+    public int Id { get; } = id;
+    public string Name { get; } = name;
+    public string Description { get; } = description;
 }
